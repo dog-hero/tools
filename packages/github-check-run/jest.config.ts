978578d7c7b35d5a0ad2e4487ebbf06a5a4d44cf
Leonardo Benedet <leonardobenedetster@gmail.com>
2022-12-27 14:43:28 -0300

    chore: config jest

---
+++ packages/github-check-run/jest.config.ts
@@ -0,0 +1,30 @@
+import type { JestConfigWithTsJest } from "ts-jest";
+
+const config: JestConfigWithTsJest = {
+  roots: ["<rootDir>/src"],
+  preset: "ts-jest/presets/js-with-ts",
+  testEnvironment: "node",
+  testMatch: ["**/?(*.)+(spec|test).+(ts|tsx|js)"],
+  transform: {
+    "^.+\\.(ts|tsx)$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.json",
+      },
+    ],
+  },
+  collectCoverage: true,
+  collectCoverageFrom: ["**/*.{ts,tsx}", "!**/node_modules/**"],
+  coverageDirectory: "<rootDir>/coverage",
+  coverageReporters: ["clover", "json", "lcov", "html"],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+};
+
+export default config;
